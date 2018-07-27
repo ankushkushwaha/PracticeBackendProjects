23bd4475cc3b611e6a5fa3782be40d865a0e60f9
Ankush Kushwaha <ankush.kushwaha@starcut.com>
2018-07-27 13:00:31 +0300

    Remove logs

--- SimpleAddUser/app/libs/Controller.php
+++ SimpleAddUser/app/libs/Controller.php
@@ -15,14 +15,9 @@ class Controller
 	function loadModel($controllerName) {
 
 		$path = "app/Model/".strtolower($controllerName)."_model.php"; // model file path
-		echo "\n$path";
-
 		require $path;
 
-		echo "\nmodelName";
-
 		$modelName = $controllerName."_Model";
-		echo "\n$modelName";
 
 		$this->model = new $modelName(); // init model
 
